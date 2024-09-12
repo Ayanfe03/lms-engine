a2f6b58473110c5d44e15a2ea696c5827422fa1f
Olukemi Fasuan <ayanfe1711@gmail.com>
2024-09-12 16:37:29 +0100

    LMS Engine classes Update

--- book.js
+++ book.js
@@ -57,49 +57,54 @@ class DigitalLibrary extends Library {
   downloadBook(isbn) {
     this.books = this.books.find((book) => book.isbn === isbn);
     if (this.books) {
-      return `You can go ahead to download ${this.books.title}`;
+      return `Downloading ${this.books.title} by ${this.books.author}...`;
     } else {
-      return `${this.books.title} is unavailable for download`;
+      return 'The book you requested for is currently not available';
     }
   }
 }
 
+// BOOK CLASS
 const person2 = new Book('The millionaire fastlane', 'MJ DeMaro', '9675438213476');
-person2.borrowBook()
+console.log(person2); // returns a Book object
 
-// console.log(person2.isbn) // returns original isbn
-// person2.isbn = "9675438213473" // updating isbn with setter method
-// console.log(person2.isbn); // returns updated isbn
-// console.log(person2); // returns available: false
+person2.borrowBook() // changes the available property of Book to false
+console.log(person2); // returns available property of class Book as false
 
-// person2.returnBook();
-// console.log(person2); // returns available: true
+person2.returnBook(); // changes the available property of Book to true
+console.log(person2); // returns available as true
 
+console.log(person2.isbn) // returns original isbn
+
+person2.isbn = "9675438213473" // updating isbn with setter method
+console.log(person2.isbn); // returns updated isbn
+
+
+// LIBRARY CLASS
 const person3 = new Library();
 
 person3.addBook({title: 'Deep Work', author: 'Cal Newport', isbn: '9347582948376'});
 person3.addBook({title: 'One up on Wall Street', author: 'Peter Lynch', isbn: '9458472927392'});
 person3.addBook({title: 'Atomic Habits', author: 'James Clear', isbn: '9539615602433'});
+console.log(person3); // returns array of book objects
 
-// console.log(person3); // returns array of book objects
+person3.removeBook('9458472927392'); // specifying book object to be removed from aray by ISBN number
+console.log(person3); // returns filtered array devoid of removed book object
 
-// person3.removeBook('9539615602433'); // specifying book object to be removed from aray by ISBN number
+person3.findBookByTitle('Deep Work'); // specifying book object to be found based on title
+console.log(person3); // returns book object with corresponding title
 
-// console.log(person3); // returns filtered array devoid of removed book object
 
-// const newBook = person3.findBookByTitle('Deep Work'); // returns book object based on title
-// console.log(newBook);
-
+// DIGITALLIBRARY CLASS INHERITING PROPERTIES OF LIBRARY CLASS
 const libraryBook = new DigitalLibrary();
 
 libraryBook.addBook({title: 'Buffettology', author: 'Mary Buffett and David Clarke', isbn: '9740274674892'});
 libraryBook.addBook({title: 'Goals!', author: 'Brian Tracy', isbn: '9463652971543'});
-
 console.log(libraryBook); // returns an array of book objects
 
-libraryBook.downloadBook('9740274674892'); // specifying which book to be downloaded based on ISBN
+const newDownload = libraryBook.downloadBook('9463652971543'); // specifying which book to be downloaded based on ISBN
+console.log(newDownload); // returns a book object with a corresponding ISBN
 
-console.log(libraryBook); // returns a book object with a corresponding ISBN
 
 
 
